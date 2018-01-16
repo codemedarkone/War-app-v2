c892e3b9f4c492716b10b11ffd30e1414d378b5c
Chris <codemedarkone@gmail.com>
2018-01-16 14:42:22 -0700

    Initial Commit

---
+++ War app v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  War app v2
+//
+//  Created by chris  on 1/16/18.
+//  Copyright © 2018 kuronuma studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
