1215a7fdaba05f053f653b1576d87d84a8fbf7de
Chris <codemedarkone@gmail.com>
2018-01-16 18:22:03 -0700

    added card randomizer to work in views

--- War app v2/ViewController.swift
+++ War app v2/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        print("left random number is: \(leftRandomNumber)")
+        
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        print("right random number is: \(rightRandomNumber)")
+        
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
+    }
+    
+    
 
 }
 
